a90cda8cc1677a0d0d29c4561c152fe7214d2f77
Tim Noonan <tnoonan@salesforce.com>
2018-10-30 13:10:46 -0600

    Updates

--- scripts/script.sh
+++ scripts/script.sh
@@ -6,7 +6,7 @@ set -x
 sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile $JWTKEYFILE --username $DEVHUB_USERNAME --setdefaultdevhubusername -a HubOrg
 
 # Create or org
-sfdx force:org:create -v HubOrg -s -f config/project-scratch-def.json -a ciorg -w 2 --loglevel trace
+sfdx force:org:create -v HubOrg -s -f config/project-scratch-def.json -a ciorg --dev-debug
 
 if [ $? -ne 0 ]; then
     bunyan ~/.sfdx/sfdx.log
@@ -14,7 +14,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # push source
-sfdx force:source:push -u ciorg && sfdx force:apex:test:run -u ciorg -c -r human
+sfdx force:source:push -u ciorg && sfdx force:apex:test:run -u ciorg -c -r human --dev-debug
 
 # delete the org
 sfdx force:org:delete -u ciorg -p
